0662c680b9c95a1aa91ee6209903a0595fae403e
ekosooo <ekoalfnt@gmail.com>
2019-10-29 13:14:38 +0700

    Form Fragment 2

--- app/src/main/java/com/example/apkersan2/fragment/IsiLaporanFragment.java
+++ app/src/main/java/com/example/apkersan2/fragment/IsiLaporanFragment.java
@@ -1,9 +1,13 @@
 package com.example.apkersan2.fragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,13 +17,33 @@ import com.example.apkersan2.R;
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class IsiLaporanFragment extends Fragment implements Step {
     View v;
+
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private EditText EtDateResult;
+    private ImageView BtDatePicker;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v =  inflater.inflate(R.layout.isilaporan_fragment, container, false);
+
+        dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        EtDateResult = v.findViewById(R.id.EtTglKejadian);
+        BtDatePicker = v.findViewById(R.id.ic_calendar);
+
+        BtDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogDate();
+            }
+        });
 
         return v;
     }
@@ -38,5 +62,20 @@ public class IsiLaporanFragment extends Fragment implements Step {
     @Override
     public void onError(@NonNull VerificationError error) {
 
+    }
+
+    private void showDialogDate(){
+
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+
+                EtDateResult.setText(dateFormatter.format(newDate.getTime()));
+            }
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
     }
 }
