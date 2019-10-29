35217d8bbb1deb4820ac7e744af8d82430f139ea
ekosooo <ekoalfnt@gmail.com>
2019-10-29 22:16:45 +0700

    Form Fragment 2

--- app/src/main/java/com/example/apkersan2/fragment/IsiLaporanFragment.java
+++ app/src/main/java/com/example/apkersan2/fragment/IsiLaporanFragment.java
@@ -1,24 +1,43 @@
 package com.example.apkersan2.fragment;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.apkersan2.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class IsiLaporanFragment extends Fragment implements Step {
@@ -27,7 +46,10 @@ public class IsiLaporanFragment extends Fragment implements Step {
     private DatePickerDialog datePickerDialog;
     private SimpleDateFormat dateFormatter;
     private EditText EtDateResult;
-    private ImageView BtDatePicker;
+    private ImageView BtDatePicker, IvBukti;
+    private TextView TvUnggah;
+
+    private int GALLERY = 1, CAMERA = 2;
 
     @Nullable
     @Override
@@ -37,6 +59,16 @@ public class IsiLaporanFragment extends Fragment implements Step {
         dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
         EtDateResult = v.findViewById(R.id.EtTglKejadian);
         BtDatePicker = v.findViewById(R.id.ic_calendar);
+
+        TvUnggah    = v.findViewById(R.id.TvUnggah);
+        IvBukti     = v.findViewById(R.id.IvBuktiKekerasan);
+
+        TvUnggah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPicturesDialog();
+            }
+        });
 
         BtDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,5 +109,98 @@ public class IsiLaporanFragment extends Fragment implements Step {
             }
         }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
         datePickerDialog.show();
+    }
+
+    private void showPicturesDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getActivity());
+        pictureDialog.setTitle("Bukti Kekerasan");
+        String[] pictureDialogItems = {
+                "Camera",
+                "Galerry"
+        };
+        pictureDialog.setItems(pictureDialogItems, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:
+                        takePhotoFromCamera();
+                        break;
+                    case 1:
+                        choosePhotoFromGalerry();
+                        break;
+                }
+            }
+        });
+        pictureDialog.show();
+    }
+
+    public void choosePhotoFromGalerry(){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    public void takePhotoFromCamera(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == getActivity().RESULT_CANCELED){
+            return;
+        }
+        if (requestCode == GALLERY && data != null){
+            Uri contentURI = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), contentURI);
+                IvBukti.setImageBitmap(bitmap);
+                IvBukti.setVisibility(View.VISIBLE);
+            }catch (IOException e){
+                e.printStackTrace();
+                Toast.makeText(getActivity(), "Failed!", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == CAMERA){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+            IvBukti.setImageBitmap(bitmap);
+        }
+    }
+
+    private void requestMultiplePermission() {
+        Dexter.withActivity(getActivity())
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+//                            Toast.makeText(getApplicationContext(), "All permissions are granted by user!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+                            //openSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getActivity(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
     }
 }
